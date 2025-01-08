718d3c3aa8de97f3471925bc3eab060a4431b4fd
Pavel Klimenko <p.klimenko@itachsoft.com>
2025-01-08 13:05:48 +0300

    added test configurations

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LuckyController extends AbstractController
+{
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('number.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
